9f44b570a0bd5ba2705b6b4871ca15a954f68c8c
Mo Helmy <helmysmile3@gmail.com>
2022-10-27 01:45:18 +0200

    first version

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,22 @@
-import express, { Request, Response } from 'express'
-import bodyParser from 'body-parser'
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+import routes from './routes';
 
-const app: express.Application = express()
-const address: string = "0.0.0.0:3000"
+const app: express.Application = express();
+const address = 'http://localhost:3000';
 
-app.use(bodyParser.json())
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+
+app.use('/api', routes);
 
 app.get('/', function (req: Request, res: Response) {
-    res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
 app.listen(3000, function () {
-    console.log(`starting app on: ${address}`)
-})
+  console.log(`starting app on: ${address}`);
+});
+
+export default app;
